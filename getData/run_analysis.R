3062bd8a73456d66eee7025b416235d0d9f1f5b8
Jaewoo Song <jaewoo.song.1225@gmail.com>
2014-07-27 02:57:49 +0800

    Update run_analysis.R
    Finished the 1st, 2nd, 3rd steps.

--- getData/run_analysis.R
+++ getData/run_analysis.R
@@ -13,3 +13,44 @@ funDown <- function() {
 }
 
 funDown()
+
+# Read the features
+features <- read.table("./data/UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+features <- features[,2]
+
+# Read the training data set
+trainSet <- read.table("./data/UCI HAR Dataset/train/X_train.txt", stringsAsFactors=FALSE)
+names(trainSet) <- features
+
+# Read the test data set
+testSet <- read.table("./data/UCI HAR Dataset/test/X_test.txt", stringsAsFactors=FALSE)
+names(testSet) <- features
+
+## Now the first step:
+## "Merges the training and the test sets to create one data set."
+totalSet <- rbind(trainSet, testSet)
+
+# Select mean() and std() only
+index <- grep("mean\\(\\)|std\\(\\)", features)
+
+## Now the second step:
+## "Extracts only the measurements on the mean and standard deviation for each measurement."
+meanStdSet <- totalSet[,index]
+
+# Read the labels
+activityLabel <- read.table("./data/UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
+trainLabel <- read.table("./data/UCI HAR Dataset/train/y_train.txt", stringsAsFactors=FALSE)
+testLabel <- read.table("./data/UCI HAR Dataset/test/y_test.txt", stringsAsFactors=FALSE)
+
+names(trainLabel) <- c("Activity")
+names(testLabel) <- c("Activity")
+
+trainLabel$Activity <- activityLabel[trainLabel[,1],2]
+testLabel$Activity <- activityLabel[testLabel[,1],2]
+fullLabel <- c(trainLabel$Activity, testLabel$Activity)
+
+## Now the third step:
+## "Uses descriptive activity names to name the activities in the data set."
+meanStdSet <- cbind(Activity=fullLabel, meanStdSet)
+
+
